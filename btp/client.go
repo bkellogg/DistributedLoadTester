1b2913a557581d635745c87c9f4a2b07afefb8ae
Brendan Kellogg <brendan6@uw.edu>
2018-04-11 16:52:45 -0700

    btp client works

---
+++ btp/client.go
@@ -0,0 +1,98 @@
+package btp
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+// RequestBuilder represents a builder
+// for BTP requests
+type RequestBuilder struct {
+	hasPayload  bool
+	beenSent    bool
+	address     string
+	file        string
+	payloadSize int64
+	response    io.ReadCloser
+}
+
+// NewRequestBuilder returns an empty RequestBuilder
+// pointed at the given address.
+func NewRequestBuilder(address string) *RequestBuilder {
+	return &RequestBuilder{hasPayload: false, address: address}
+}
+
+// SetFile sets the given file path as the file
+// to send when the request is sent.
+func (rb *RequestBuilder) SetFile(filepath string) {
+	rb.hasPayload = true
+	rb.file = filepath
+}
+
+// Send sends the RequestBuilder's request.
+// Returns an error if one occurred.
+func (rb *RequestBuilder) Send() error {
+	if !rb.hasPayload {
+		return fmt.Errorf("btp: cannot send a request with no body")
+	}
+	if rb.beenSent {
+		return fmt.Errorf("btp: cannot sent a request that has already been sent")
+	}
+
+	// open the connection to this request builder's
+	// address. Report any errors that occur.
+	conn, err := net.Dial("tcp", rb.address)
+	if err != nil {
+		return fmt.Errorf("btp: error dialing address: %v", rb.address)
+	}
+
+	// open the file of this request builder
+	// report any errors that occur.
+	f, err := os.Open(rb.file)
+	if err != nil {
+		conn.Close()
+		return fmt.Errorf("btp: error opening file: %v", err)
+	}
+
+	// obtain the file statistic so we can get the size of the file
+	// report any errors that occur.
+	fstat, err := f.Stat()
+	if err != nil {
+		conn.Close()
+		return fmt.Errorf("btp: error obtaining file statistics: %v", err)
+	}
+
+	// write the size of the file to the connection
+	// report any errors that occur.
+	if err = binary.Write(conn, binary.LittleEndian, fstat.Size()); err != nil {
+		conn.Close()
+		return fmt.Errorf("btp: error writing file size: %v", err)
+	}
+
+	// copy the contents of the file into the connection
+	// report any errors that occur.
+	if _, err = io.Copy(conn, f); err != nil {
+		conn.Close()
+		return fmt.Errorf("btp: error copying file into connection: %v", err)
+	}
+
+	rb.response = conn
+	rb.beenSent = true
+
+	return nil
+}
+
+// Response gets the response readCloser from the connection.
+// Returns an error if one occurred.
+func (rb *RequestBuilder) Response() (io.ReadCloser, error) {
+	if !rb.beenSent {
+		return nil, fmt.Errorf("btp: cannot get the response of a request that has not been sent")
+	}
+	if rb.response == nil {
+		return nil, fmt.Errorf("btp: response is nil")
+	}
+	return rb.response, nil
+}
--- btpclient/main.go
+++ btpclient/main.go
@@ -1,5 +1,57 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/BKellogg/DistributedLoadTester/btp"
+)
+
+const poorSucker = "localhost:8080"
+
 func main() {
 
+	if len(os.Args) < 2 {
+		log.Fatal(`usage:
+			btpclient <app_path>`)
+	}
+	appPath := os.Args[1]
+	fullPath, err := filepath.Abs(appPath)
+	if err != nil {
+		log.Fatalf("error getting absolute path: %v", err)
+	}
+
+	reqBuilder := btp.NewRequestBuilder(poorSucker)
+	reqBuilder.SetFile(fullPath)
+	if err := reqBuilder.Send(); err != nil {
+		log.Fatalf("error sending request: %v", err)
+	}
+
+	response, err := reqBuilder.Response()
+	if err != nil {
+		log.Fatalf("error getting response: %v", err)
+	}
+	defer response.Close()
+
+	// Read from the conneciton until the connection closes.
+	if err = copyFromRCIntoStdOut(response); err != nil {
+		log.Fatalf("error printing from readcloser: %v", err)
+	}
+}
+
+// copyRCConnIntoStdOut reads and prints all messages from the readcloser
+// until the process is terminated, an error occurs, or the connection is
+// closed.
+func copyFromRCIntoStdOut(rc io.ReadCloser) error {
+	fmt.Printf("==== Start of connection read ====\n\n")
+	numBytes, err := io.Copy(os.Stdout, rc)
+	fmt.Printf("\n\n==== End of connection read ====\n")
+	fmt.Printf("total bytes read: %d", numBytes)
+	if err != nil && err != io.EOF {
+		return fmt.Errorf("error copying into connection into standard out: %v", err)
+	}
+	return nil
 }
